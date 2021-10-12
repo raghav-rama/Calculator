d728b1b9a06d8827aa84825d16c8d219306865e0
raghav-rama <hackerer528@gmail.com>
2021-10-12 14:13:10 +0530

    custom mini-library for extracting numbers and operations

---
+++ app/src/main/java/com/example/calculator/expression_evaluator/main/Resources.java
@@ -0,0 +1,9 @@
+package com.example.calculator.expression_evaluator.main;
+
+import java.math.BigDecimal;
+
+public interface Resources {
+    String divideByZero = "Can't divide by Zero";
+    // char[] ops = new char[]{' ', '/', '*', '+', '-'};
+    // int[] ops_priority = {-1, 1, 2, 3, 4};
+}
